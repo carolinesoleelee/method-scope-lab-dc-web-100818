86a00169686beffc7c4204084814704fdc27ec85
Solee Lee <carolinesoleelee@gmail.com>
2018-09-19 23:03:54 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,14 +3,11 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-
+def catch_phrase
+  puts phrase
 
 
 ## Comment the below back in to see method scope in action!
 
 # rescue_princess_peach
 # puts status
-
-
-
-
